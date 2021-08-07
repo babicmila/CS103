ffbd8dde00f27496ae99bd8d94f4189f169e25e7
Mila Babic <milababic5@gmail.com>
2021-08-07 10:58:19 +0200

    exercise4 commit

---
+++ src/cs103/project/MedianOfTwoSortedArrays.java
@@ -0,0 +1,46 @@
+package cs103.project;
+
+/*
+Data su dva sortirana niza nums1 and nums2 dužina m i n po redosledu. 
+Vratiti medijanu ova dva sortirana niza.
+Ukupna vremenska složenost treba biti O(log (m+n)).
+ */
+public class MedianOfTwoSortedArrays {
+
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int x = nums1.length;
+        int y = nums2.length;
+
+        int low = 0;
+        int high = x;
+        while (low <= high) {
+            int partitionX = (low + high) / 2;
+            int partitionY = (x + y + 1) / 2 - partitionX;
+
+            int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : nums1[partitionX - 1];
+            int minRightX = (partitionX == x) ? Integer.MAX_VALUE : nums1[partitionX];
+
+            int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : nums2[partitionY - 1];
+            int minRightY = (partitionY == y) ? Integer.MAX_VALUE : nums2[partitionY];
+
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                
+                if ((x + y) % 2 == 0) {
+                    return ((double) Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY)) / 2;
+                } else {
+                    return (double) Math.max(maxLeftX, maxLeftY);
+                }
+            } else if (maxLeftX > minRightY) {
+                high = partitionX - 1;
+            } else { 
+                low = partitionX + 1;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+
+}
