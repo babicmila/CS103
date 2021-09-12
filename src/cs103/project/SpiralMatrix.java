8e7f9132a6533e36d8d3fe1b1743b312d07a715d
Mila Babic <milababic5@gmail.com>
2021-09-12 14:48:51 +0200

    exercise6 commit

--- src/cs103/project/SpiralMatrix.java
+++ src/cs103/project/SpiralMatrix.java
@@ -10,21 +10,54 @@ public class SpiralMatrix {
 
     public List<Integer> spiralOrder(int[][] matrix) {
 
+        List<Integer> elements = new ArrayList<>();
+
+        if (matrix == null || matrix.length == 0) {
+            return elements;
+        }
+
         int rows = matrix.length;
         int columns = matrix[0].length;
-        List<Integer> newMatrix = new ArrayList<>();
-        int size = newMatrix.size();
+        int matrixSize = matrix.length * matrix[0].length;
+        int arraySize = elements.size();
 
-        int upBoundary = 0;
+        int topBoundary = 0;
         int downBoundary = rows - 1;
         int leftBoundary = 0;
         int rightBoundary = columns - 1;
 
-        while (size < rows * columns) {
+        while (topBoundary <= downBoundary && leftBoundary <= rightBoundary) {
 
+            for (int i = leftBoundary; i <= rightBoundary; i++) {
+                elements.add(matrix[topBoundary][i]);
+            }
+            topBoundary++;
+
+            for (int i = topBoundary; i <= downBoundary; i++) {
+                elements.add(matrix[i][rightBoundary]);
+            }
+
+            rightBoundary--;
+
+            if (topBoundary <= downBoundary) {
+
+                for (int i = rightBoundary; i >= leftBoundary; i--) {
+                    elements.add(matrix[downBoundary][i]);
+                }
+
+                downBoundary--;
+            }
+            
+            if(leftBoundary <= rightBoundary){
+                
+                for(int i = downBoundary; i>=topBoundary; i--){
+                    elements.add(matrix[i][leftBoundary]);
+                }
+                leftBoundary++;
+            }
+            
         }
-
-        return newMatrix;
+        return elements;
     }
 
 }
