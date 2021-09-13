8645da36b8fdc259d043e9a6c65148d1a022dd56
Mila Babic <milababic5@gmail.com>
2021-09-13 02:11:25 +0200

    exercise7 commit

--- src/cs103/project/RotateList.java
+++ src/cs103/project/RotateList.java
@@ -1,14 +1,61 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cs103.project;
 
 /**
- *
- * @author milab
+ * Data je “glava” (“head”) jednostruko povezane liste. Rotirati listu u desno
+ * za k mesta.
  */
 public class RotateList {
-    
+
+    // Definition for singly-linked list.
+    public class ListNode {
+
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode rotateRight(ListNode head, int k) {
+
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        if (k == 0) {
+            return head;
+        }
+
+        int size = 1;
+        ListNode tail = head;
+
+        while (tail.next != null) {
+
+            tail = tail.next;
+            size++;
+        }
+
+        tail.next = head;
+
+        size = size - (k % size);
+
+        for (int i = 0; i < size; i++) {
+            tail = tail.next;
+            head = head.next;
+        }
+
+        tail.next = null;
+
+        return head;
+    }
+
 }
